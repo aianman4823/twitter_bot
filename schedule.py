49e1cbb3d4f7c095fa295cc58c8b590f46893892
aianman4823 <akitoaianman1@gmail.com>
2019-07-30 16:23:04 +0900

    change schedule to add retweet_job

--- schedule.py
+++ schedule.py
@@ -8,14 +8,18 @@ def timed_job():
     autolike.liketweet()
     autoretweet.autoretweet()
 
+def retweet_job():
+    autoretweet.autoretweet()
+
 def remove_job():
     removefollow.remove()
 
 
 if __name__=="__main__":
     scheduler = BlockingScheduler()
-    scheduler.add_job(timed_job, 'cron',hour=7,minute=10)
-    scheduler.add_job(remove_job,'cron',hour=7,minute=15)
+    scheduler.add_job(timed_job, 'cron',hour=7,minute=0)
+    scheduler.add_job(retweet_job,'cron',hour=12,minute=0)
+    scheduler.add_job(remove_job,'interval',weeks=1)
 
     try:
         scheduler.start()
